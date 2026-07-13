f3595b54f100584af0e9b56b5a2fef74f1e49cc1
hopfion <hopfion@tutamail.com>
2026-07-14 01:38:02 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE="nikki-v11";
+var CACHE="nikki-v12";
 var ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./icon-512-maskable.png","./apple-touch-icon.png"];
 self.addEventListener("install",function(e){
   e.waitUntil(caches.open(CACHE).then(function(c){
